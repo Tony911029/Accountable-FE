54b083c294a095cb10131cdf5800547e537addfe
Tony Chan <tony.chan@sciteline.com>
2024-01-21 21:00:40 -0500

    testing

--- src/navigation/Auth/UserPoolConfig.js
+++ src/navigation/Auth/UserPoolConfig.js
@@ -1,4 +1,9 @@
+// export const cognitoConfig = {
+//     UserPoolId: import.meta.env.VITE_USER_POOL_ID,
+//     ClientId: import.meta.env.VITE_CLIENT_ID
+// }
+
 export const cognitoConfig = {
-    UserPoolId: import.meta.env.VITE_USER_POOL_ID,
-    ClientId: import.meta.env.VITE_CLIENT_ID
+    UserPoolId: "ca-central-1_7V5ipZjOx",
+    ClientId: "22m42371ush9bo4md0no3rf2ip"
 }
