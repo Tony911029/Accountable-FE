7bab172db6f3b5b791fb7e41c43542b9474f9056
Tony Chan <tony.chan@sciteline.com>
2024-01-21 22:23:18 -0500

    replace api key

--- src/navigation/Auth/UserPoolConfig.js
+++ src/navigation/Auth/UserPoolConfig.js
@@ -1,9 +1,4 @@
-// export const cognitoConfig = {
-//     UserPoolId: import.meta.env.VITE_USER_POOL_ID,
-//     ClientId: import.meta.env.VITE_CLIENT_ID
-// }
-
 export const cognitoConfig = {
-    UserPoolId: "ca-central-1_7V5ipZjOx",
-    ClientId: "22m42371ush9bo4md0no3rf2ip"
+    UserPoolId: import.meta.env.VITE_USER_POOL_ID,
+    ClientId: import.meta.env.VITE_CLIENT_ID
 }
