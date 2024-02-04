5a1fc042283f6a45bce2867316a7e7ba889f85a3
Tony Chan <tony.chan@sciteline.com>
2024-02-04 16:03:19 -0500

    add eslint

--- src/pages/Login/LoadingPage.js
+++ src/pages/Login/LoadingPage.js
@@ -1,13 +1,15 @@
-import {PacmanLoader} from "react-spinners";
+import { PacmanLoader } from 'react-spinners';
 
 export default function LoadingPage() {
-    return (<PacmanLoader
-        color="#FF9900"
-        size={"5rem"}
-        style={{
-            position: 'fixed',
-            top: '30%',
-            left: '45%',
-        }}
-    />)
+  return (
+    <PacmanLoader
+      color="#FF9900"
+      size="5rem"
+      style={{
+        position: 'fixed',
+        top: '30%',
+        left: '45%',
+      }}
+    />
+  );
 }
